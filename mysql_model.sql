d5a3df72ec25413466dbb12d759736e8aba2e4f1
Kite1874 <kite18741998@163.com>
2020-12-16 20:03:05 +0800

    更新数据库模型

--- mysql_model.sql
+++ mysql_model.sql
@@ -1,3 +1,19 @@
+CREATE TABLE `category` (
+`cid` int(11) NOT NULL AUTO_INCREMENT COMMENT '类目ID',
+`name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '类目名',
+`pid` int(11) NOT NULL DEFAULT 0 COMMENT '父ID',
+PRIMARY KEY (`cid`) 
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 11
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+COMMENT = '分类表'
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
 CREATE TABLE `d_admin` (
 `a_id` int(11) NOT NULL AUTO_INCREMENT,
 `a_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -24,14 +40,13 @@ CREATE TABLE `d_article` (
 `a_viewcount` int(11) NOT NULL,
 `a_likecount` int(11) NOT NULL,
 `a_banner` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-`a_status` int(11) NULL DEFAULT NULL,
+`a_status` smallint(1) NOT NULL DEFAULT 0,
 `u_id` int(11) NOT NULL,
-`cat_id` int(11) NOT NULL,
 PRIMARY KEY (`a_id`) ,
 INDEX `fk_article_article_1` (`u_id` ASC) USING BTREE
 )
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 4
 AVG_ROW_LENGTH = 0
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_general_ci
@@ -39,7 +54,7 @@ KEY_BLOCK_SIZE = 0
 MAX_ROWS = 0
 MIN_ROWS = 0
 ROW_FORMAT = Dynamic;
-CREATE TABLE `d_article_catorgery_relationship` (
+CREATE TABLE `d_article_category_relationship` (
 `acr_id` int(11) NOT NULL AUTO_INCREMENT,
 `cat_id` int(11) NOT NULL,
 `a_id` int(11) NOT NULL,
@@ -48,7 +63,7 @@ INDEX `fk_d_article_catorgery_relationship_d_article_catorgery_relatio1` (`a_id`
 INDEX `fk_d_article_catorgery_relationship_d_article_catorgery_relatio2` (`cat_id` ASC) USING BTREE
 )
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 5
 AVG_ROW_LENGTH = 0
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_general_ci
@@ -56,17 +71,19 @@ KEY_BLOCK_SIZE = 0
 MAX_ROWS = 0
 MIN_ROWS = 0
 ROW_FORMAT = Dynamic;
-CREATE TABLE `d_catorgery` (
+CREATE TABLE `d_category` (
 `cat_id` int(11) NOT NULL AUTO_INCREMENT,
-`cat_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-`cat_parentid` int(11) NULL DEFAULT NULL,
-`cat_type` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-`cat_order` int(11) NOT NULL,
+`cat_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+`cat_parentid` int(11) NULL DEFAULT 0,
+`cat_type` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'category',
+`cat_order` int(11) NOT NULL DEFAULT 1,
 `cat_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`cat_level` smallint(1) NOT NULL DEFAULT 1,
+`cat_status` smallint(1) NOT NULL DEFAULT 0,
 PRIMARY KEY (`cat_id`) 
 )
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 117
 AVG_ROW_LENGTH = 0
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_general_ci
@@ -127,14 +144,31 @@ KEY_BLOCK_SIZE = 0
 MAX_ROWS = 0
 MIN_ROWS = 0
 ROW_FORMAT = Dynamic;
+CREATE TABLE `d_like` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`type_id` int(11) NOT NULL,
+`type` smallint(6) NOT NULL,
+`u_id` int(11) NOT NULL,
+`status` smallint(1) NOT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Dynamic;
 CREATE TABLE `d_user` (
 `u_id` int(11) NOT NULL AUTO_INCREMENT,
 `u_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 `u_password` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 `u_role` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'normal',
-`u_sex` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`u_sex` smallint(1) NOT NULL DEFAULT 0,
 `u_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-`u_status` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
+`u_status` smallint(1) NOT NULL DEFAULT 0,
 `u_create_time` datetime NOT NULL,
 PRIMARY KEY (`u_id`) 
 )
@@ -149,8 +183,8 @@ MIN_ROWS = 0
 ROW_FORMAT = Dynamic;
 
 ALTER TABLE `d_article` ADD CONSTRAINT `fk_article_article_1` FOREIGN KEY (`u_id`) REFERENCES `d_user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `d_article_catorgery_relationship` ADD CONSTRAINT `fk_d_article_catorgery_relationship_d_article_catorgery_relatio1` FOREIGN KEY (`a_id`) REFERENCES `d_article` (`a_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `d_article_catorgery_relationship` ADD CONSTRAINT `fk_d_article_catorgery_relationship_d_article_catorgery_relatio2` FOREIGN KEY (`cat_id`) REFERENCES `d_catorgery` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `d_article_category_relationship` ADD CONSTRAINT `fk_d_article_catorgery_relationship_d_article_catorgery_relatio1` FOREIGN KEY (`a_id`) REFERENCES `d_article` (`a_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `d_article_category_relationship` ADD CONSTRAINT `fk_d_article_catorgery_relationship_d_article_catorgery_relatio2` FOREIGN KEY (`cat_id`) REFERENCES `d_category` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `d_comment` ADD CONSTRAINT `fk_d_comment_d_comment_1` FOREIGN KEY (`u_id`) REFERENCES `d_user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `d_favorites` ADD CONSTRAINT `fk_d_favorites_d_favorites_1` FOREIGN KEY (`u_id`) REFERENCES `d_user` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `d_favorites` ADD CONSTRAINT `fk_d_favorites_d_favorites_2` FOREIGN KEY (`a_id`) REFERENCES `d_article` (`a_id`) ON DELETE CASCADE ON UPDATE CASCADE;
